86e99a22a8d1075960fd28ec74b7d30ae8141566
hongchal <chal405@naver.com>
2021-02-20 01:34:22 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class App extends React.Component{
-  
+  state = {
+    count : 0
+  };
+  add = () => {
+    // console.log("add");
+    console.log("add");
+  }
+  minus = () => {
+    console.log("minus");
+  }
+
+  render(){
+    return (
+    <div>
+      <h1>the number is : {this.state.count}</h1>
+      <button onClick = {this.add}>Add</button>
+      <button onClick = {this.minus}>Minus</button>
+
+    </div>
+    );
+  }
+
 }
 
 export default App; 
